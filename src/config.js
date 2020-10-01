fcf1fd48da08ce4ac5d525daa4d9ad78f88cbb32
AleMicky <miguel.ale19934@gmail.com>
2020-10-01 09:41:34 -0400

    pro

--- src/config.js
+++ src/config.js
@@ -1,6 +1,7 @@
-const apiURL = 'http://localhost:1337/';
-const apiImg = 'http://localhost:1337';
-
+// const apiURL = 'http://localhost:1337/';
+// const apiImg = 'http://localhost:1337';
+const apiURL = 'https://banck-end.herokuapp.com/';
+const apiImg = 'https://banck-end.herokuapp.com';
 export {
     apiURL,
     apiImg
