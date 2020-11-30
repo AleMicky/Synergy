1c7fd210837f463f5e4667fd45f56bca2d5bbcdc
AleMicky <miguel.ale19934@gmail.com>
2020-11-29 20:43:56 -0400

    nuevo protocolo

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 // const apiURL = 'http://localhost:1337/';
 // const apiImg = 'http://localhost:1337';
-const apiURL = 'http://backend.yacelly.ml:1337/';
-const apiImg = 'http://backend.yacelly.ml:1337';
+const apiURL = 'https://node.yacelly.ml/';
+const apiImg = 'https://node.yacelly.ml';
 export {
     apiURL,
     apiImg
